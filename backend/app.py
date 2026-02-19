451358d80b16a5b031445bc7b17cb94e6667e964
Sanjana-Malkhede <sanjanamalkhede111@gmail.com>
2026-02-19 16:28:23 +0530

    Fixed utils import path

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from utils import predict_habitability
+from backend.utils import predict_habitability
 import pandas as pd
 
 app = Flask(__name__)
