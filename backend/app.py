b43da8f283cbe5987f21b26f0c2b10b2bbbe1966
Kala Venkata Naga Sri Yasaswini <kalasriyasaswini@gmail.com>
2026-02-20 19:12:55 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import joblib
 import numpy as np
 import pandas as pd
+import os
 
 
 from utils import validate_input, format_prediction
@@ -278,5 +279,7 @@ def download_predicted():
 # ===============================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
